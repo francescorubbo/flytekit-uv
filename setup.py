9c0ae12594c502c89a33f82308aeba36c85cf45b
Francesco Rubbo <frubbo@altoslabs.com>
2025-06-25 17:34:18 +0200

    update deps

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 PLUGIN_NAME = "uv"
 
 microlib_name = f"flytekitplugins-{PLUGIN_NAME}"
-plugin_requires = ["flytekit>=1.16.1,<2.0.0"]
+plugin_requires = ["flytekit>=1.16,<2"]
 
 __version__ = "0.0.0+develop"
 
@@ -19,14 +19,14 @@ setup(
     namespace_packages=["flytekitplugins"],
     packages=[f"flytekitplugins.{PLUGIN_NAME}"],
     install_requires=plugin_requires,
-    license="apache2",
-    python_requires=">=3.9",
+    license="MIT",
+    python_requires=">=3.12",
     classifiers=[
         "Intended Audience :: Science/Research",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Topic :: Scientific/Engineering",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Topic :: Software Development",
